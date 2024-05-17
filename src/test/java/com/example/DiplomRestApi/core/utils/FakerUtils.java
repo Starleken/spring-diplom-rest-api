4ba8bd5ccebdc2e775bfac54efba700903b1c1ad
unknown <starleken@mail.ru>
2024-05-17 08:24:16 +0700

    add init data change

--- src/test/java/com/example/DiplomRestApi/core/utils/FakerUtils.java
+++
@@ -1,8 +0,0 @@
-package com.example.DiplomRestApi.core.utils;
-
-import com.github.javafaker.Faker;
-
-public abstract class FakerUtils {
-
-    public static Faker FAKER = new Faker();
-}
--- src/test/java/com/example/DiplomRestApi/core/utils/dto/ActivityDtoUtils.java
+++
@@ -1,9 +0,0 @@
-package com.example.DiplomRestApi.core.utils.dto;
-
-import com.example.DiplomRestApi.core.utils.FakerUtils;
-import com.example.DiplomRestApi.dto.activity.ActivityCreateDto;
-
-import static com.example.DiplomRestApi.core.utils.FakerUtils.*;
-
-public abstract class ActivityDtoUtils {
-}
